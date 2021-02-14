efd17cc38d6d9915de8865097bb9dbcb8fcb9eee
habibullah49 <habibullahsheikh640@gmail.com>
2021-02-14 23:42:39 +0600

    complete project

--- app.js
+++ app.js
@@ -24,18 +24,8 @@ const showImages = (images) => {
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div)
-    toggleSpinner(false);
   })
 
-}
-
-const toggleSpinner = (show) => {
-  const spinner = document.getElementById('loading-spinner');
-  if (show) {
-    spinner.classList.remove('d-none');
-  }else{
-    spinner.classList.add('d-none');
-  }
 }
 
 const getImages = (query) => {
@@ -44,7 +34,6 @@ const getImages = (query) => {
     .then(response => response.json())
     .then(data => showImages(data.hits))
     .catch(err => console.log(err))
-    toggleSpinner(true);
 }
 
 let slideIndex = 0;
